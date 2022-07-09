8137b8631774ea393141bde40c0f97921424d841
JHawke <107356280+JHawke16@users.noreply.github.com>
2022-07-09 10:46:01 +0000

    It now reacts whenver clown is spoken and replies that it is a clown as well

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ async def on_ready():
   print('Bot is ready')
 
 @client.event
-async def on_message(message):
-  if message.author == client.user:
+async def on_message(msg):
+  if msg.author == client.user:
     return
 
-  if message.content == 'clown':
-    await message.add_reaction('\U0001F921')
+  if msg.content == 'clown':
+    await msg.add_reaction('\U0001F921')
 
+  if 'clown' in msg.content.lower():
+    await msg.add_reaction('\U0001F921')
+    await msg.channel.send('I am a clown too  \U0001F921')
 
 client.run(os.environ['TOKEN'])
 
